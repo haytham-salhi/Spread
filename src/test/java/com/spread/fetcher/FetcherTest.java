89c3bc67eb7265420d4f21f72367f54c3d34e4d7
Haytham Salhi <hsalhi89@gmail.com>
2016-10-22 02:32:10 +0300

    Test Jenkins

--- src/test/java/com/spread/fetcher/FetcherTest.java
+++ src/test/java/com/spread/fetcher/FetcherTest.java
@@ -54,7 +54,7 @@ public class FetcherTest {
 
 	@Before
 	public void setUp() throws Exception {
-		System.out.println("Hello Jenkinssss");
+		System.out.println("Hello Jenkinsssss");
 	}
 	
 	@Test
