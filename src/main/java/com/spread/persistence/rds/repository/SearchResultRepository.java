50e6cd49036004d619c38c30c11e0cde91136a42
Haytham Salhi <hsalhi89@gmail.com>
2017-04-16 01:14:24 +0200

    Extends the search result repository

--- src/main/java/com/spread/persistence/rds/repository/SearchResultRepository.java
+++ src/main/java/com/spread/persistence/rds/repository/SearchResultRepository.java
@@ -2,6 +2,7 @@ package com.spread.persistence.rds.repository;
 
 import java.util.List;
 
+import com.spread.frontcontrollers.labeling.model.YesNoAnswer;
 import com.spread.persistence.rds.model.SearchResult;
 import com.spread.persistence.rds.model.enums.Location;
 import com.spread.persistence.rds.model.enums.SearchEngineCode;
@@ -83,6 +84,23 @@ public interface SearchResultRepository extends CrudRepository<SearchResult, Int
 			+ "AND userSearchResultAssessment.isRelevant = 'YES'")
 	List<SearchResult> findAgreedRelevantByQueryAndSearchEngine(@Param("queryId") Integer queryId, @Param("code") SearchEngineCode code, @Param("location") Location location, @Param("language") SearchEngineLanguage language, Pageable pageable);
 	
+	// this shoud not be here
+	// To understand this query, see query # 4 and 5 in assessement.sql
+	@Query("SELECT new com.spread.persistence.rds.model.SearchResult(searchResult.id, searchResult.title, searchResult.url, searchResult.snippet) "
+			+ "FROM UserSearchResultAssessment userSearchResultAssessment "
+			+ "JOIN userSearchResultAssessment.searchResult searchResult" 
+			+ ", UserSearchResultAssessment ua2 " // cross join
+			+ "JOIN searchResult.querySearchEngine querySearchEngine "
+			+ "JOIN querySearchEngine.query query "
+			+ "JOIN querySearchEngine.searchEngine searchEngine "
+			+ "WHERE ua2.searchResult.id = userSearchResultAssessment.searchResult.id " // cross join
+			+ "AND query.id = :queryId AND searchEngine.code = :code AND searchEngine.location = :location AND searchEngine.language= :language "
+			+ "AND userSearchResultAssessment.user.id != ua2.user.id "
+			+ "AND userSearchResultAssessment.isRelevant = ua2.isRelevant "
+			+ "AND userSearchResultAssessment.user.id = :firstJudgeId AND ua2.user.id = :secondJudgeId "
+			+ "AND userSearchResultAssessment.isRelevant = :isRelevant")
+	List<SearchResult> findAgreedByQueryAndSearchEngine(@Param("queryId") Integer queryId, @Param("firstJudgeId") Integer firstJudgeId, @Param("secondJudgeId") Integer secondJudgeId, @Param("isRelevant") YesNoAnswer isRelevant, @Param("code") SearchEngineCode code, @Param("location") Location location, @Param("language") SearchEngineLanguage language, Pageable pageable);
+	
 	@Query("SELECT userSearchResultAssessment.searchResult "
 			+ "FROM UserSearchResultAssessment userSearchResultAssessment "
 			+ "JOIN userSearchResultAssessment.searchResult searchResult "
@@ -96,5 +114,16 @@ public interface SearchResultRepository extends CrudRepository<SearchResult, Int
 			+ "AND searchResult.innerPage IS NOT NULL") // Those having innerpage
 	List<SearchResult> findRelevantArabicWithInnerPagesByQueryAndSearchEngine(@Param("queryId") Integer queryId, @Param("code") SearchEngineCode code, @Param("location") Location location, @Param("language") SearchEngineLanguage language, Pageable pageable);
 	
-	
+	@Query("SELECT new com.spread.persistence.rds.model.SearchResult(searchResult.id, searchResult.title, searchResult.url, searchResult.snippet) "
+			+ "FROM UserSearchResultAssessment userSearchResultAssessment "
+			+ "JOIN userSearchResultAssessment.searchResult searchResult "
+			+ "JOIN searchResult.querySearchEngine querySearchEngine "
+			+ "JOIN querySearchEngine.searchEngine searchEngine "
+			+ "JOIN querySearchEngine.query query "
+			+ "WHERE query.id = :queryId AND searchEngine.code = :code AND searchEngine.location = :location AND searchEngine.language= :language "
+			+ "AND userSearchResultAssessment.user.id = :userId "
+			+ "AND userSearchResultAssessment.isRelevant = 'YES' "
+			+ "AND regexp(searchResult.title, '[؟-ي]+') = 1 " // Those in arabic
+			+ "AND searchResult.innerPage IS NOT NULL") // Those having innerpage
+	List<SearchResult> findRelevantArabicWithInnerPagesByQueryAndSearchEngine(@Param("queryId") Integer queryId, @Param("userId") Integer userId,@Param("code") SearchEngineCode code, @Param("location") Location location, @Param("language") SearchEngineLanguage language, Pageable pageable);
 }
