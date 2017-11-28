116dcbeccefcd82f08be3baa92a30c14512dd89a
haytham <hsalhi89@gmail.com>
2017-11-28 16:38:46 +0200

    Implement item5 API

--- src/main/java/com/spread/frontcontrollers/ExperimentAPIController.java
+++ src/main/java/com/spread/frontcontrollers/ExperimentAPIController.java
@@ -22,6 +22,7 @@ import weka.core.stemmers.Stemmer;
 import com.spread.experiment.data.Data;
 import com.spread.experiment.data.stemmers.LightStemmer;
 import com.spread.experiment.experiments.AQDynamicKExperiment;
+import com.spread.experiment.experiments.AQSupervisedExperiment;
 import com.spread.experiment.experiments.BaseExperiment;
 import com.spread.experiment.experiments.EnhancedCQExperiment;
 import com.spread.experiment.experiments.EnhancedCQExperiment2;
@@ -177,6 +178,7 @@ public class ExperimentAPIController implements Serializable {
 	}
 	
 	/**
+	 * This API for item4 experiments
 	 */
 	@RequestMapping(value = "/item4run", method = RequestMethod.GET)
 	public ResponseEntity<String> item4Run(
@@ -264,6 +266,94 @@ public class ExperimentAPIController implements Serializable {
 		return new ResponseEntity<String>(new File(".").getAbsolutePath(), HttpStatus.OK);
 	}
 	
-	// TODO implement item5run API
-	
+	/**
+	 * This API for item5 experiments
+	 */
+	@RequestMapping(value = "/item5run", method = RequestMethod.GET)
+	public ResponseEntity<String> item5Run(
+			@RequestParam(defaultValue= "1", required = false, name = "aqst") int aqSizeTest,
+			@RequestParam(defaultValue = "40", required = false, name = "wtk1") int wordsToKeep,
+			@RequestParam(defaultValue = "300", required = false, name = "wtk2") int wordsToKeepInCaseOfInnerPage,
+			// Size per clear query (for training data)
+			@RequestParam(defaultValue = "30", required = false, name = "size") int size,
+			// Possible values: G (for Google), B (for Bing), and GB (for Google and Bing)
+			@RequestParam(defaultValue = "G", required = false, name = "engine") String engine,
+			// Appended to the fo
+			@RequestParam(defaultValue = "", required = false, name = "customName") String customName) {
+		
+		BaseExperiment aQSupervisedExperiment = (BaseExperiment) applicationContext.getBean("aQSupervisedExperiment");
+		
+		// 1.
+		aQSupervisedExperiment.setExperimentName("experiment-item5-dynamic-k-full_" + true + "-" + new Date().getTime() + "-" + customName); // The folder name of the experiment
+		aQSupervisedExperiment.setAlgorithmName("k-means"); // the sub folder name of the experiment
+		aQSupervisedExperiment.setBasePath("/var/www/html/experiments/"); // To be set just here in APIs
+		aQSupervisedExperiment.setSizeOfAmbiguousQueriesToLoaded(aqSizeTest);
+		
+		// 2.
+		// Variables
+		int[] sizes = {size};
+		FeatureSelectionModes[] featureSelectionModes = {FeatureSelectionModes.TITLE_ONLY,
+				FeatureSelectionModes.SNIPPET_ONLY,
+				FeatureSelectionModes.TITLE_WITH_SNIPPET,
+				FeatureSelectionModes.INNER_PAGE}; // Mainly we change this in this experiment
+		FeatureSpaceModes[] featureSpaceModes = {FeatureSpaceModes.SINGLE_WORDS, 
+				FeatureSpaceModes.PHRASES_OF_TWO_GRAMS,
+				FeatureSpaceModes.SINGLE_WORDS_WITH_TWO_GRAMS,
+				FeatureSpaceModes.SINGLE_WORDS_WITH_TWO_GRAMS_WITH_THREE_GRAMS};
+		
+		// These are usually neutralized
+		SearchEngineCode searchEngineCode = SearchEngineCode.GOOGLE;
+		switch (engine) {
+			case "G":
+				searchEngineCode = SearchEngineCode.GOOGLE;
+				break;
+			case "B":
+				searchEngineCode = SearchEngineCode.BING;
+				break;
+			case "GB":
+				searchEngineCode = null;
+				break;
+			default:
+				experimentRunning = false;
+				return new ResponseEntity<String>("Weein raye7 ya kbeer", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		boolean withInnerPage = true;
+		
+		// Text preprocessing related
+		Stemmer stemmer = new LightStemmer(); 
+		boolean letterNormalization = true;
+		boolean diacriticsRemoval = true;
+		boolean puncutationRemoval = true;
+		boolean nonArabicWordsRemoval = true;
+		boolean arabicNumbersRemoval = true;
+		boolean nonAlphabeticWordsRemoval = true;
+		boolean stopWordsRemoval = true;
+		boolean ambiguousQueryRemoval = true;
+		
+		// Vector-space related (dictionary related)
+		boolean countWords = true;
+		//int wordsToKeep = 40; // the top-N most common words;
+		//int wordsToKeepInCaseOfInnerPage = 300; // Only applied when detecting innerPage attribute added to training set
+		boolean TF = false; // damping
+		boolean IDF = true;
+		int nGramMinSize = 1; // 1 and 1 mean tokenize 1 gram (1 word), 2 and 2 mean toenize 2-gram words 
+		int nGramMaxSize = 1; // If you specify a range 1, 2. That means 1-gram and 2-gram will be included in the dictionary (lexicon)
+		int minTermFreqToKeep = 1; // TODO think about it?
+		
+		((AQSupervisedExperiment)aQSupervisedExperiment).setVariables(sizes, featureSelectionModes, featureSpaceModes, searchEngineCode, withInnerPage, stemmer, letterNormalization, diacriticsRemoval, puncutationRemoval, nonArabicWordsRemoval,
+				arabicNumbersRemoval, nonAlphabeticWordsRemoval, stopWordsRemoval, ambiguousQueryRemoval,
+				countWords, wordsToKeep, wordsToKeepInCaseOfInnerPage, TF, IDF, minTermFreqToKeep);
+		
+		// 3.
+		try {
+			aQSupervisedExperiment.run();
+		} catch (Exception e1) {
+			LOGGER.error(ExceptionUtils.getStackTrace(e1));
+			
+			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<String>(new File(".").getAbsolutePath(), HttpStatus.OK);
+	}
 }
